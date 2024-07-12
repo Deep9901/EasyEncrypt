20b8c0ef3e580f897ef22c9f238a07714a71536a
Subham Das <114517693+Deep9901@users.noreply.github.com>
2024-07-12 12:39:14 +0530

    open and read input files added

--- encryption.cpp
+++ encryption.cpp
@@ -1,8 +1,32 @@
 #include "encryption.h"
 #include <fstream>
 #include <cctype>
+#include <iterator>
+#include <string>
 
-bool encryptFile(const std::string &filename, bool encrypt);
+
+bool performCaesarCipher(std::string &content, bool encrypt)
 {
     
 }
+
+bool encryptFile(const std::string &filename, bool encrypt)
+{
+    // Open the input file
+    std::ifstream inFile(filename);
+    if (!inFile)
+    {
+        return false;
+    }
+
+    // Read the content of the file
+    std::string content(istreambuf_iterator<char>(inFile), {})
+    inFile.close();
+
+    if (performCaesarCipher(content, encrypt)) 
+    {
+
+    }
+}
+
+ 
