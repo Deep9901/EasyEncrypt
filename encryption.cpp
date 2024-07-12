cd3e55ea767063525ad6053ab4e83c1e8b1de4ac
Subham Das <114517693+Deep9901@users.noreply.github.com>
2024-07-12 12:43:31 +0530

    Caesar Cipher function added

--- encryption.cpp
+++ encryption.cpp
@@ -7,7 +7,18 @@
 
 bool performCaesarCipher(std::string &content, bool encrypt)
 {
-    
+    std::int shift = encrypt ? 3 : -3;
+
+    for (std::char &ch: content)
+    {
+        if (isalpha(ch))
+        {
+            std::char base = isupper(ch) ? 'A' : 'B';
+            ch = static_cast<char>((ch - base + shift + 26) % 26 + base);
+        }
+
+        return true;
+    }
 }
 
 bool encryptFile(const std::string &filename, bool encrypt)
